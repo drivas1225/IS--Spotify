37f85459e7f61b2c0d109903e4c8f2e2b17f4e92
Edwin Daniel Lazarinos Vilca <edwin.lazarinos@ucsp.edu.pe>
2017-11-04 13:15:46 -0500

    Ranking

---
+++ spootify/src/main/java/com/example/demo/Ranking.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Ranking {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id_ranking;
+	
+	@Column(length = 64)
+	private String nombre;
+	
+	@Column
+	private Date fecha;
+	
+
+}
