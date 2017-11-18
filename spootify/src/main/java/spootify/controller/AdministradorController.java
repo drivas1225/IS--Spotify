9753d35cd4fd634cadf1d1278dc62a8813140e08
Jesamin30 <31374312+Jesamin30@users.noreply.github.com>
2017-11-18 10:32:59 -0500

    ADMINISTRADORCONTROLLER
    JESAMIN ZEVALLOS

--- spootify/src/main/java/spootify/controller/AdministradorController.java
+++ spootify/src/main/java/spootify/controller/AdministradorController.java
@@ -1,12 +1,23 @@
 package spootify.controller;
 
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
 import com.example.demo.Administrador;
 import spootify.service.AdministradorService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Collection;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 
 @Controller
 @RequestMapping("/administrador")
@@ -14,7 +25,24 @@ import spootify.service.AdministradorService;
 public class AdministradorController {
 	@Autowired
 	AdministradorService admiService;
+	private String user;
 	
+	@RequestMapping(value="/login_adm",method=RequestMethod.POST)
+	@ResponseBody
+	Boolean login_adm(@RequestBody Administrador admin) throws Exception{
+		System.out.println("guardando: "+admin.getNickname());
+		user=admin.getNickname();
+		System.out.println("guardando!!");
+		//admiService.login_adm(admin.getNickname(),admin.getPassword());
+		return Boolean.TRUE;
+		
+	}
+	
+	//@RequestMapping("/session_adm")
+	//@ResponseBody
+	//Administrador session_adm() {
+		//return admiService.session_adm(user);
+	//}
 	
 
 }
