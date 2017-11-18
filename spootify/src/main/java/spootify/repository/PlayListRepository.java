f5dad6a406959d53a9063118b9dd60572ad3203f
Jesamin30 <31374312+Jesamin30@users.noreply.github.com>
2017-11-18 10:18:41 -0500

    Jesamin_Z
    Playlist repository

--- spootify/src/main/java/spootify/repository/PlayListRepository.java
+++ spootify/src/main/java/spootify/repository/PlayListRepository.java
@@ -1,5 +1,8 @@
 package spootify.repository;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -7,7 +10,6 @@ import com.example.demo.Album;
 import com.example.demo.PlayList;
 import com.example.demo.Usuario;
 
-import antlr.collections.List;
 
 public interface PlayListRepository  extends CrudRepository<PlayList, Long>{
 	@Query("SELECT a FROM PlayList a WHERE a.ususario=?1")
