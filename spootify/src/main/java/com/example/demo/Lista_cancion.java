7f4d7f4d8f377a3ba19c22968b74a2d57fdf2642
diego rivas <diego.rivas@ucsp.edu.pe>
2017-11-28 09:17:41 -0500

    ćorreciones

--- spootify/src/main/java/com/example/demo/Lista_cancion.java
+++ spootify/src/main/java/com/example/demo/Lista_cancion.java
@@ -1,6 +1,11 @@
 package com.example.demo;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Entity
 public class Lista_cancion {
 	@Id
